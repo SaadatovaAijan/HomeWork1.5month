ba15d84ad7412b001ef1f6b406a1786d75ede050
saadatovaaijan@icloud.com <saadatovaaijan@icloud.com>
2024-06-17 11:35:19 +0300

    Initial Commit

---
+++ HomeWork1.5month/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork1.5month
+//
+//  Created by Aijan Saadatova on 17/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
